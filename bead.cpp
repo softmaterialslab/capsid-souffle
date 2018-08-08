6210a94f64da8369991a8d7efcaa48710f3ebecf
lauren <lm44@iu.edu>
2018-08-08 14:50:33 -0400

    rearranged harmonic stretching

--- bead.cpp
+++ bead.cpp
@@ -11,7 +11,7 @@ void BEAD::update_stretching_energy(double ks, double bondlength)
     se = 0;
     for(int i=0;i< (itsE.size() );i++)      //Using 1.5 for ~5nm edge length in protein
     {
-        se += ( 0.125 * ks * ( itsE[i]->len0-itsE[i]->length ) * ( itsE[i]->len0-itsE[i]->length ) );
+        se += ( 0.25 * ks * ( itsE[i]->len0-itsE[i]->length ) * ( itsE[i]->len0-itsE[i]->length ) );
     }
 }
 
@@ -21,6 +21,6 @@ void BEAD::update_stretching_force(double ks, double bondlength)
     for(int i=0; i< itsE.size(); i++)
     {
         VECTOR3D r_vec = dist(this, (itsE[i]->opposite(this)));
-        sforce += ((r_vec) ^ ((itsE[i]->len0 - itsE[i]->length) * 0.5*ks / itsE[i]->length));
+        sforce += ((r_vec) ^ ((itsE[i]->len0 - itsE[i]->length) * ks / itsE[i]->length));
     }
 }
