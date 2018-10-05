07b6d2a701701b4e151c107b0655e36e507d4902
lauren <lm44@iu.edu>
2018-10-05 12:24:47 -0400

    updated examples, removed unused functions

--- forces.cpp
+++ forces.cpp
@@ -12,184 +12,12 @@
 
 using namespace std;
 
-void update_ES_forces_pairlist(vector<BEAD>& subunit_bead, double lb, double ni, double qs, vector<PAIR>& lj_pairlist){
-	for (unsigned int i=0; i<subunit_bead.size(); i++){
-		subunit_bead[i].eforce = VECTOR3D(0,0,0);
-	}
-	
-	for (unsigned int i=0; i<lj_pairlist.size(); i++){
-		VECTOR3D r_vec = dist( lj_pairlist[i].itsB[0] , lj_pairlist[i].itsB[1] );
-		long double r = r_vec.GetMagnitude();
-		double kappa = 8 * 3.1416 * ni * lb * qs*qs;
-		VECTOR3D ff = r_vec ^ ( ( lj_pairlist[i].itsB[0]->q * lj_pairlist[i].itsB[1]->q * lb * exp(-kappa * r )
-		/ (r * r) ) * (kappa + 1/r) );
-		
-		lj_pairlist[i].itsB[0]->eforce += ff;
-		lj_pairlist[i].itsB[1]->eforce -= ff;
-	}
-	
-}
 
-void update_ES_forces_intra(vector<SUBUNIT>& protein, double lb, double ni, double qs){
-	for (unsigned int i = 0; i < protein.size(); i++){
-		for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++)							//intramolecular forces loop
-		{
-			for (unsigned int kk = ii + 1; kk < protein[i].itsB.size(); kk++)
-			{
-				double kappa = 8 * 3.1416 * ni * lb * qs*qs;
-				VECTOR3D r_vec = dist( protein[i].itsB[ii] , protein[i].itsB[kk] );
-				long double r = r_vec.GetMagnitude();
-				VECTOR3D ff = r_vec ^ ( ( protein[i].itsB[ii]->q * protein[i].itsB[kk]->q * lb * exp(-kappa * r )
-                          / (r*r) ) * (kappa + 1/r ) );
 
-				protein[i].itsB[ii]->eforce += ff;
-				protein[i].itsB[kk]->eforce -= ff;
-			}
-		}
-	}
-}
 
-void update_ES_forces(vector<SUBUNIT>& protein, double lb, double ni, double qs){
-    for (unsigned int i=0; i<protein.size(); i++){
-		for (unsigned int ii=0; ii<protein[i].itsB.size(); ii++)
-		{
-			protein[i].itsB[ii]->eforce = VECTOR3D(0,0,0);
-		}
-    }
-    
-    for (unsigned int i = 0; i < protein.size() ; i++)									//intermolecular forces loop
-	{
-		for (unsigned int j = 0; j < protein.size(); j++)
-		{
-			if (protein[i].id != protein[j].id){
-				for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++)
-				{
-					for (unsigned int jj = 0; jj < protein[j].itsB.size(); jj++)
-					{
-						double kappa = 8 * 3.1416 * ni * lb * qs*qs;
-						VECTOR3D r_vec = dist( protein[i].itsB[ii] , protein[j].itsB[jj] );
-						long double r = r_vec.GetMagnitude();
-						VECTOR3D ff = r_vec ^ ( ( protein[i].itsB[ii]->q * protein[j].itsB[jj]->q * lb * exp(-kappa * r )
-						/ (r * r) ) * (kappa + 1/r) );
-						
-						
-						protein[i].itsB[ii]->eforce += ff;
-						
-					}
-				}
-			}
-			
-		}
-		for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++)							//intramolecular forces loop
-		{
-			for (unsigned int kk = ii + 1; kk < protein[i].itsB.size(); kk++)
-			{
-				double kappa = 8 * 3.1416 * ni * lb * qs*qs;
-				VECTOR3D r_vec = dist( protein[i].itsB[ii] , protein[i].itsB[kk] );
-				long double r = r_vec.GetMagnitude();
-				VECTOR3D ff = r_vec ^ ( ( protein[i].itsB[ii]->q * protein[i].itsB[kk]->q * lb * exp(-kappa * r )
-                          / (r*r) ) * (kappa + 1/r ) );
 
-				protein[i].itsB[ii]->eforce += ff;
-				protein[i].itsB[kk]->eforce -= ff;
-			}
-		}
-	}
-	
-	
- }
+
  
- void update_LJ_forces_pairlist(vector<SUBUNIT>& protein, double ecut, vector<PAIR>& lj_pairlist){
-	 
-	 for (unsigned int i = 0; i<protein.size(); i++) {
-		 for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++){
-			 protein[i].itsB[ii]->ljforce = VECTOR3D(0,0,0);
-		 }
-	 }
-	 
-	 for (unsigned int i=0; i<lj_pairlist.size(); i++){
-		 
-		 VECTOR3D r_vec = dist( lj_pairlist[i].itsB[0] , lj_pairlist[i].itsB[1] );
-		 long double r = r_vec.GetMagnitude();
-		 //double r2 = (r*r);
-		 double r6 ;
-		 double r12;
-		 double sigma6;
-		 double shc = 1.2;
-		 double elj = lj_pairlist[i].epsilon;
-		 double sig1 = lj_pairlist[i].itsB[0]->sigma;
-		 double sig2 = lj_pairlist[i].itsB[1]->sigma;
-		 double del = (sig1+sig2)/2 - shc;
-		 
-		 if (lj_pairlist[i].type==0 && r < (del+1.12246205*shc)){
-			 sigma6 = pow(shc,6);
-			 double sigma12 = sigma6 * sigma6;
-			 r6 = pow((r-del),6);
-			 r12 = r6 * r6;
-			 lj_pairlist[i].itsB[0]->ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del))) ));
-			 lj_pairlist[i].itsB[1]->ljforce -= (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del))) ));
-		 } else if (lj_pairlist[i].type==1 && r < ((del+1.12246205*shc)*ecut)){
-			 r6 = pow((r-del),6);
-			 r12 = r6 * r6;
-			 sigma6 = pow(shc,6);
-			 double sigma12 = sigma6 * sigma6;
-			 lj_pairlist[i].itsB[0]->ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del)))));
-			 lj_pairlist[i].itsB[1]->ljforce -= (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del)))));
-		 } else {
-			 lj_pairlist[i].itsB[0]->ljforce += 0;
-			 lj_pairlist[i].itsB[1]->ljforce += 0;
-		 }
-	 }
- }
-
-void update_LJ_forces(vector<SUBUNIT>& protein, double ecut, vector<PAIR>& lj_pairlist){
-
-	for (unsigned int i = 0; i < protein.size(); i++){
-		for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++){
-			protein[i].itsB[ii]->ljforce = VECTOR3D(0,0,0);
-		}
-	}
-	
-	
-		for (unsigned int i=0; i < protein.size(); i++)
-		{
-			for (unsigned int ii=0; ii<protein[i].itsB.size(); ii++)
-				{
-					for (unsigned int n = 0; n < protein[i].itsB[ii]->itsP.size(); n++)
-						{
-								VECTOR3D r_vec = dist( protein[i].itsB[ii] , protein[i].itsB[ii]->itsP[n]->itsB[1] );
-								if ( r_vec.GetMagnitude() == 0) r_vec = dist( protein[i].itsB[ii] , protein[i].itsB[ii]->itsP[n]->itsB[0] );
-								long double r = r_vec.GetMagnitude();
-								//double r2 = (r*r);
-								double r6 ;
-								double r12;
-								double sigma6;
-								double shc = 1.2;
-								double elj = protein[i].itsB[ii]->itsP[n]->epsilon;
-								double sig1 = protein[i].itsB[ii]->itsP[n]->itsB[1]->sigma;
-								double sig2 = protein[i].itsB[ii]->itsP[n]->itsB[0]->sigma;
-								double del = (sig1+sig2)/2 - shc;
-
-								if (protein[i].itsB[ii]->itsP[n]->type == 0 && r < (del+1.12246205*shc) ){
-									sigma6 = pow(shc,6);
-									double sigma12 = sigma6 * sigma6;
-									r6 = pow((r-del),6);
-									r12 = r6 * r6;
-									protein[i].itsB[ii]->ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del))) ));
-								} else if (protein[i].itsB[ii]->itsP[n]->type == 1 && r < ((del+1.12246205*shc)*ecut) ){
-									r6 = pow((r-del),6);
-									r12 = r6 * r6;
-									sigma6 = pow(shc,6);
-									double sigma12 = sigma6 * sigma6;
-									protein[i].itsB[ii]->ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del)))));
-								} else {
-									protein[i].itsB[ii]->ljforce += 0;
-								}
-							//}
-						}
-				}
-		}
-}
 
 void update_ES_forces_simplified(vector<BEAD>& subunit_bead, double lb, double ni, double qs)
  {
@@ -218,10 +46,23 @@ void update_ES_forces_simplified(vector<BEAD>& subunit_bead, double lb, double n
 	 
  }
  
- void update_LJ_forces_simplified(vector<BEAD>& subunit_bead, double ecut, vector<vector<int> > lj_a){
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ void update_LJ_forces_simplified(vector<BEAD>& subunit_bead, double rcut, vector<vector<int> > lj_a){
 	 for (unsigned int i = 0; i < subunit_bead.size(); i++){
 		 subunit_bead[i].ljforce = VECTOR3D(0,0,0);
 	 }
+	 
+	 double shc = 1.2;
+	 double shc6 = shc * shc * shc * shc * shc * shc;
+	 double shc12 = shc6 * shc6;
+	 
 	 for (unsigned int i = 0; i < subunit_bead.size(); i++){
 		 for (unsigned int j = 0; j < subunit_bead.size(); j++){
 			 if (subunit_bead[i].itsS[0]->id != subunit_bead[j].itsS[0]->id){
@@ -230,8 +71,6 @@ void update_ES_forces_simplified(vector<BEAD>& subunit_bead, double lb, double n
 				 //double r2 = (r*r);
 				 double r6 ;
 				 double r12;
-				 double sigma6;
-				 double shc = 1.2;
 				 
 				 double sig1 = subunit_bead[i].sigma;
 				 double sig2 = subunit_bead[j].sigma;
@@ -243,19 +82,15 @@ void update_ES_forces_simplified(vector<BEAD>& subunit_bead, double lb, double n
 					 }
 				 }
 				 if ( r < (del+1.12246205*shc) && lj_attractive == false){							//Repulsive
-					 sigma6 = pow(shc,6);
-					 double sigma12 = sigma6 * sigma6;
 					 double elj = 1;//subunit_bead[j].epsilon;
-					 r6 = pow((r-del),6);
+					 r6 = (r-del) * (r-del) * (r-del) * (r-del) * (r-del) * (r-del);
 					 r12 = r6 * r6;
-					 subunit_bead[i].ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del))) ));
-				 } else if ( r < ((del+1.12246205*shc)*ecut) && lj_attractive == true ){			//Attractive
-					 r6 = pow((r-del),6);
+					 subunit_bead[i].ljforce += (r_vec ^ (48 * elj * ((shc12 / r12) - 0.5 * (shc6 / r6)) * (1 / (r*(r-del))) ));
+				 } else if ( r < ((del+1.12246205*shc)*rcut) && lj_attractive == true ){			//Attractive
+					 r6 = (r-del) * (r-del) * (r-del) * (r-del) * (r-del) * (r-del);
 					 r12 = r6 * r6;
-					 sigma6 = pow(shc,6);
-					 double sigma12 = sigma6 * sigma6;
 					 double elj = 2;//subunit_bead[j].epsilon;
-					 subunit_bead[i].ljforce += (r_vec ^ (48 * elj * ((sigma12 / r12) - 0.5 * (sigma6 / r6)) * (1 / (r*(r-del)))));
+					 subunit_bead[i].ljforce += (r_vec ^ (48 * elj * ((shc12 / r12) - 0.5 * (shc6 / r6)) * (1 / (r*(r-del)))));
 				 } else {
 					 subunit_bead[i].ljforce += 0;
 				 }
