5e3fae79bbdf006ecf2b9945af440c7997207543
Fanbo Sun <35780487+fanbsun@users.noreply.github.com>
2024-01-23 18:24:50 -0500

    update make file

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int analyze_output(int, char **);
 
 int main(int argc, char *argv[]) {
-    if (boost::filesystem::remove_all("outfiles") == 0) perror("Error deleting outfiles directory");
-    else cout << "Pre-existing outfiles directory successfully, replaced with an empty directory." << endl;
-    boost::filesystem::create_directory("outfiles");
+    //if (boost::filesystem::remove_all("outfiles") == 0) perror("Error deleting outfiles directory");
+    //else cout << "Pre-existing outfiles directory successfully, replaced with an empty directory." << endl;
+    //boost::filesystem::create_directory("outfiles");
 
 	run_simulation(argc,argv);
     analyze_output(argc,argv);
