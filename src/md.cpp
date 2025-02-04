901fe0f12383081a65cebfffe2eec83393449710
Fanbo Sun <fanbsun@indiana.edu>
2025-02-04 15:13:24 -0500

    Update md.cpp

--- src/md.cpp
+++ src/md.cpp
@@ -439,8 +439,8 @@ int run_simulation(int argc, char *argv[]) {
 
             for (unsigned int i = 0; i < protein.size(); i++) {
                for (unsigned int ii = 0; ii < protein[i].itsB.size(); ii++) {
-                  protein[i].itsB[ii]->sforce = protein[i].itsB[ii]->sforce - sforce_drift[i];
-                  protein[i].itsB[ii]->bforce = protein[i].itsB[ii]->bforce - bforce_drift[i];
+                  protein[i].itsB[ii]->sforce = protein[i].itsB[ii]->sforce - (sforce_drift[i]/protein[i].itsB.size());
+                  protein[i].itsB[ii]->bforce = protein[i].itsB[ii]->bforce - (bforce_drift[i]/protein[i].itsB.size());
                }
             }
 
